a19850cd52136d7034de16f762a0b93cf26f02db
Ndriqim Haxhaj <ndriqim@zombiesoup.com>
2020-04-07 23:13:32 +0200

    Main view design

--- OneKeyDev/OneKeyDev/Controller/ViewController.swift
+++ OneKeyDev/OneKeyDev/Controller/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    
+    // MARK: - Properties
 
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    // MARK: - Functions
+    @IBAction func backgroundTapped(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
 }
 
